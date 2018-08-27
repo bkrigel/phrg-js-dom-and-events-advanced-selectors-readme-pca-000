d0892dd7004588351e5373ecb36b97f3ba420da6
Benjamin Krigel <benjamin.krigel@gmail.com>
2018-08-26 21:59:00 -0400

    not sure i understand the :first-child

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector() {
+  const result = $('p')
+  return result
+}
+
+function lastImageSelector() {
+  const result = $('img:last')
+  return result
+}
+
+function ninjaBabySelector() {
+  const result = $('#baby-ninja')
+  return result
+}
+
+function divSelector() {
+  const result = $('.pics')
+  return result
+}
+
+function firstListItem() {
+  const result = $('#pic-list li:first')
+  return result
+}
